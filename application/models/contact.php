a7bf2e3909152a2f388eca538b99438fea5712a8
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2013-11-05 16:15:16 +0200

    yazim hatasi

--- application/models/contact.php
+++ application/models/contact.php
@@ -3,7 +3,7 @@
 
 		public function create(){
 			$this->created_at = date("Y-m-d H:i:s");
-			$this->ip_addres = $this->input->ip_address();
+			$this->ip_address = $this->input->ip_address();
 			$this->db->insert("Contact", $this);
 			return $this->db->insert_id();
 		}
