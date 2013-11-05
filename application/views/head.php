8307cdf14bc34e8bd6446eeff27c71e0c7eb8d6e
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2013-11-06 01:55:34 +0200

    hede

--- application/views/head.php
+++ application/views/head.php
@@ -48,7 +48,7 @@
 		});
 	</script>
 	<style type="text/css">
-		<?php if($this->agent->platform() == "Windows"){?>
+		<?php if($this->agent->platform() != "Mac OS X"){?>
 			.page h2, .page .body p, .btn-lg, .textarea textarea, .input input::-moz-placeholder, .input input::-webkit-input-placeholder, .input input, #tiptip_holder, .juri-detay .body h2 span, .juri-detay .title {
 				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif !important;
 				font-weight: 100 !important;
