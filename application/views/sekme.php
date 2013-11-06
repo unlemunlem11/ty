254fb4ecd4de0cbf8d5ca5b4572de4f98ec755b8
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2013-11-06 21:22:09 +0200

    as

---
+++ application/views/sekme.php
@@ -0,0 +1,13 @@
+<?php include("head.php");?>
+	<title>Bridgestone Tasarım Yarışması</title>
+
+</head>
+<body>
+
+<div style="width:810px; height: 700px; float:left; background: url('<?php echo base_url() ?>/img/sekme-bg.jpg');">
+
+	<a href="#" class="btn-lg" style="text-decoration:none; float: left;margin-top: 336px;margin-left: 275px;">Katıl</a>
+
+</div>
+
+<?php include("footer.php");?>
