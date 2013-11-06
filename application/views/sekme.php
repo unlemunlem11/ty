f15e187d3b4dd62a03274bd9353415ed9bc6a77a
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2013-11-06 21:38:52 +0200

    as

--- application/views/sekme.php
+++ application/views/sekme.php
@@ -34,7 +34,7 @@
 					if (response.authResponse) {
 						userdata['access_token_expire_date'] = toDateTime(FB.getAuthResponse()['expiresIn']);
 						userdata['access_token'] = FB.getAuthResponse()['accessToken'];
-						location.href="https://apps.facebook.com/casecontest/";
+						top.location.href="https://apps.facebook.com/casecontest/";
 					}
 				}, {scope: 'user_likes,user_about_me,user_interests,user_education_history,user_work_history,email,user_birthday,user_hometown,user_location,user_relationships,user_relationship_details,user_website'});
 			});
