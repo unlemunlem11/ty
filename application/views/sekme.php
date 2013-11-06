4b7206d5cd7718e6df2959a9d0509b5d6e52beec
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2013-11-06 21:34:53 +0200

    as

--- application/views/sekme.php
+++ application/views/sekme.php
@@ -1,6 +1,20 @@
 <?php include("head.php");?>
 	<title>Bridgestone Tasarım Yarışması</title>
 	<script type="text/javascript">
+		if (document.getElementById('facebook-jssdk')) {return;}
+		var firstScriptElement = document.getElementsByTagName('script')[0];
+		var facebookJS = document.createElement('script'); 
+		facebookJS.id = 'facebook-jssdk';
+		facebookJS.src = '//connect.facebook.net/en_US/all.js';
+		firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+
+		var userdata = {};
+
+		function toDateTime(secs){
+		    var t = new Date();
+		    t.setSeconds(secs);
+		    return t;
+		}
 		$(document).ready(function(){
 			$("#katil").click(function(){
 				FB.login(function(response) {
